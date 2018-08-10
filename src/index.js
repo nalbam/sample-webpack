b2c108db3201d81f16526bb6245314705d6fd44a
nalbam <me@nalbam.com>
2018-08-10 18:20:36 +0900

    logo

--- src/index.js
+++ src/index.js
@@ -6,6 +6,5 @@ import './style.scss';
 const { x, y, ...z } = { x: 1, y: 2, a: 3, b: 4 };
 const n = { x, y, ...z };
 if (Object.keys(n).map((key) => n[key]).reduce((p,v) => p + v) === 10) {
-  document.querySelector('#app').insertAdjacentHTML('afterbegin', '<h1><img src="https://cdn.nalbam.com/logo/nginx.png"></h1>');
-  document.querySelector('#app').insertAdjacentHTML('afterbegin', '<h3 id="host">sample-webpack</h3>');
+  document.querySelector('#app').insertAdjacentHTML('afterbegin', '<img src="https://cdn.nalbam.com/logo/webpack.png">');
 }
